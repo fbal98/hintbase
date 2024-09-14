6df18b34e03371ba09055cb4dca598df4980126d
Firas <firass269@gmail.com>
2024-09-14 11:10:17 +0400

    done with MVP

--- auth.ts
+++ auth.ts
@@ -2,24 +2,19 @@ import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import { SupabaseAdapter } from "@auth/supabase-adapter";
 
-if (!process.env.NEXT_PUBLIC_AUTH_SECRET) {
-  throw new Error(
-    "NEXT_PUBLIC_AUTH_SECRET is not set in the environment variables"
-  );
+if (!process.env.AUTH_SECRET) {
+  throw new Error("AUTH_SECRET is not set in the environment variables");
 }
 
-if (
-  !process.env.NEXT_PUBLIC_AUTH_GITHUB_ID ||
-  !process.env.NEXT_PUBLIC_AUTH_GITHUB_SECRET
-) {
+if (!process.env.AUTH_GITHUB_ID || !process.env.AUTH_GITHUB_SECRET) {
   throw new Error(
     "GitHub OAuth credentials are not set in the environment variables"
   );
 }
 
 if (
-  !process.env.NEXT_PUBLIC_SUPABASE_URL ||
-  !process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY
+  !process.env.AUTH_SUPABASE_URL ||
+  !process.env.AUTH_SUPABASE_SERVICE_ROLE_KEY
 ) {
   throw new Error(
     "Supabase credentials are not set in the environment variables"
@@ -29,13 +24,13 @@ if (
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     GitHub({
-      clientId: process.env.NEXT_PUBLIC_AUTH_GITHUB_ID,
-      clientSecret: process.env.NEXT_PUBLIC_AUTH_GITHUB_SECRET,
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
     }),
   ],
   adapter: SupabaseAdapter({
-    url: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    secret: process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY,
+    url: process.env.AUTH_SUPABASE_URL,
+    secret: process.env.AUTH_SUPABASE_SERVICE_ROLE_KEY,
   }),
-  secret: process.env.NEXT_PUBLIC_AUTH_SECRET,
+  secret: process.env.AUTH_SECRET,
 });
