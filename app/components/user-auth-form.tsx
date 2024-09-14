eb44c3f8d2b3c70df2ae236f7c4907915de0388c
Firas <firass269@gmail.com>
2024-09-14 11:36:14 +0400

    done with MVP

--- app/components/user-auth-form.tsx
+++ app/components/user-auth-form.tsx
@@ -63,6 +63,7 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
         type="button"
         disabled={isLoading}
         onClick={() => {
+          "use server";
           setIsLoading(true);
           signIn("github");
         }}
--- components/user-auth-form.tsx
+++ components/user-auth-form.tsx
@@ -1,8 +1,6 @@
-"use client";
-
 import * as React from "react";
 import { cn } from "@/lib/utils";
-import SignIn from "@/components/SignIn";
+import SignIn from "@/app/components/SignIn";
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
